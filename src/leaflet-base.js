db60542778328a4ac7c39124707fe70dd70738a9
Barış Arıburnu <barisariburnu@hotmail.com>
2016-11-14 16:13:15 +0300

    Add layers all

--- src/leaflet-base.js
+++ src/leaflet-base.js
@@ -1,7 +1,15 @@
 /*** Constructor ***/
-baseMaps = {};
+var baseMaps = {};
+var layers = [];
+var map;
 var countriesLayer;
 
+/*** Layers ***/
+for (var providerId in providers) {
+    layers.push(providers[providerId]);
+}
+// Add layers all
+L.control.iconLayers(layers).addTo(map);
 
-var map = L.map('map').setView([40.198868, 29.057695], 5);
+map = L.map('map').setView([40.198868, 29.057695], 5);
 countriesLayer = L.geoJson(countries).addTo(map);
