e844bc252dad995f8dc9aa7ef8120f81d9e0006f
Barış Arıburnu <barisariburnu@hotmail.com>
2016-11-15 11:54:19 +0300

    Add leaflet plugins  - Add locate control  - Add icon pulse

--- src/leaflet-base.js
+++ src/leaflet-base.js
@@ -3,8 +3,9 @@ var layers = [];
 var countriesLayer;
 var weatherControl;
 var iconLayerControl;
+var locateControl;
 
-var map = L.map('map', { zoomControl: false }).setView([40.1670987,29.1081638], 10);
+var map = L.map('map', { zoomControl: false }).setView([40.1670987,29.1081638], 4);
 
 /*** Tile Layers ***/
 for (var providerId in providers) {
@@ -16,3 +17,4 @@ countriesLayer = L.geoJson(countries).addTo(map);
 /*** Controls ***/
 iconLayerControl = L.control.iconLayers(layers).addTo(map);
 weatherControl = L.control.weather(weatherOptions).addTo(map);
+locateControl = L.control.locate(locateOptions).addTo(map);
--- src/options.js
+++ src/options.js
@@ -1,4 +1,6 @@
 var weatherOptions;
+var pulsingIconOptions;
+var locateOptions;
 
 /*** Weather ***/
 weatherOptions = {
@@ -6,4 +8,26 @@ weatherOptions = {
     units: "metric",
     position: "bottomright",
     appId: "6363ab9cb278e1e1c5d190abbc673bf9"
+};
+
+/*** Pulse Icon ***/
+pulsingIconOptions = {
+    iconSize: [15, 15],
+    color: 'red',
+    animate: true,
+    heatbeat: 1
+};
+
+/*** Locate ***/
+locateOptions = {
+    position: 'topleft',
+    drawCircle: false,
+    showPopup: true,
+    strings: {
+        title: "Konumum",
+        metersUnit: "metre",
+        feetUnit: "feet",
+        popup: "Bu noktaya {distance} {unit} çapta uzaklıktasın!",
+        outsideMapBoundsMsg: "Haritanın sınırlarının dışında görünüyorsun!"
+    }
 };
